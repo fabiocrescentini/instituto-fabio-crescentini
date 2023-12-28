37b49a0f9171025498057ce1a5793425e1019418
João Teles <98963988+joaotelesk@users.noreply.github.com>
2023-12-28 13:54:43 -0300

    :sparkles: feat: adding color and typography tokens

---
+++ src/tokens/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+  red: {
+    200: '#FF4763',
+    300: '#DA003F',
+    700: '#9E183F',
+  },
+  pink: {
+    50: '#FEF1F3',
+    100: '#FFDCE1',
+  },
+  yellow: {
+    50: '#FFFDEE',
+    100: '#F8F5E1',
+    150: '#FDFDF8',
+    400: '#EBD966',
+  },
+  gray: {
+    100: '#FBFBFB',
+    200: '#DDDDDD',
+  },
+
+  blue: '#001F3F',
+  white: '#FFFFFF',
+}
---
+++ src/tokens/typography.ts
@@ -0,0 +1,32 @@
+export const typography = {
+  fontSizes: {
+    xxs: '0.875rem',
+    xs: '0.9375rem',
+    sm: '1rem',
+    md: '1.125rem',
+    lg: '1.25rem',
+    xl: '1.875rem',
+    '1xl': '3.125rem',
+    '2xl': '3.75rem',
+  },
+  fontWeights: {
+    light: '400',
+    extraLight: '450',
+    normal: '500',
+    extraNormal: '550',
+    medium: '600',
+    extraMedium: '650',
+    bold: '700',
+    extraBold: '750',
+  },
+  lineHeights: {
+    heading: '1.25',
+    base: '1.6',
+  },
+  fontFamilies: {
+    inter: '--font-inter',
+    merriweather: '--font-merriweather',
+    lato: '--font-lato',
+    source: '--font-source',
+  },
+}
