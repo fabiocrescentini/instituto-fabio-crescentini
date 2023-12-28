d5717e42d1e309959b66d213516443c71cb736c6
João Teles <98963988+joaotelesk@users.noreply.github.com>
2023-12-28 13:57:32 -0300

    :books: docs: Updating configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { colors, typography } from './src/tokens'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -5,13 +6,25 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-      },
+      backgroundImage: {},
+    },
+
+    colors: { ...colors, current: 'currentColor' },
+    fontWeight: typography.fontWeights,
+    fontSize: typography.fontSizes,
+    lineHeight: typography.lineHeights,
+    fontFamily: {
+      inter: `var(${typography.fontFamilies.inter})`,
+      merriweather: `var(${typography.fontFamilies.merriweather})`,
+      lato: `var(${typography.fontFamilies.lato})`,
+      source: `var(${typography.fontFamilies.source})`,
     },
   },
+
   plugins: [],
 }
 export default config
