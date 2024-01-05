9b6a67e0561518b04169b8d43e1475c488c2d2c6
João Teles <98963988+joaotelesk@users.noreply.github.com>
2024-01-04 23:04:50 -0300

    :sparkles: feat: Component DivSection and ArrowRight

---
+++ src/components/common/div-sections/index.tsx
@@ -0,0 +1,32 @@
+export function DivSections() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="2"
+      viewBox="0 0 1402 2"
+      fill="none"
+    >
+      <path
+        d="M1 1L1401 1"
+        stroke="url(#paint0_linear_142_13882)"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_142_13882"
+          x1="13.25"
+          y1="1"
+          x2="1401"
+          y2="1.0002"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#9E183F" stop-opacity="0" />
+          <stop offset="0.490013" stop-color="#9E183F" />
+          <stop offset="1" stop-color="#9E183F" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
---
+++ src/components/common/icons/arrow-right.tsx
@@ -0,0 +1,16 @@
+export function ArrowRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={31}
+      height={9}
+      viewBox="0 0 31 9"
+      fill="none"
+    >
+      <path
+        d="M30.354 4.854a.5.5 0 000-.708L27.172.964a.5.5 0 10-.707.708L29.293 4.5l-2.828 2.828a.5.5 0 10.707.708l3.182-3.182zM0 5h30V4H0v1z"
+        fill="#F8F5E1"
+      />
+    </svg>
+  )
+}
